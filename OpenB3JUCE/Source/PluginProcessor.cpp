4cdf52fbc86f627b7717eb5557dfba11c1c482dc
Michele Perrone <perrone.michele@outlook.com>
2022-12-29 21:35:17 +0100

    OpenB3: implement AudioProcessorValueTreeState loading and saving

--- OpenB3JUCE/Source/PluginProcessor.cpp
+++ OpenB3JUCE/Source/PluginProcessor.cpp
@@ -188,12 +188,19 @@ void OpenB3AudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void OpenB3AudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary(data, sizeInBytes));
+    if (xmlState.get() != nullptr)
+        if(xmlState->hasTagName(apvts.state.getType()))
+            apvts.replaceState(juce::ValueTree::fromXml(*xmlState));
 }
 
 //==============================================================================
@@ -339,7 +346,7 @@ void OpenB3AudioProcessor::parameterChanged (const String &parameterID, float ne
         else if(parameterID.endsWithChar('1'))
         {
             pedalboard_drawbars[1] = apvts.getRawParameterValue("DRAWBAR_LOWER_0")->load();
-            pedalboard_drawbars[1] = (uint32_t)newValue;            
+            pedalboard_drawbars[1] = (uint32_t)newValue;
         }
         beatrix->set_drawbars(PEDAL_BOARD, pedalboard_drawbars);
     }
